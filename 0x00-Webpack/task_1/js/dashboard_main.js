dc8ea23d4273e61f918747e49185c5b79c99bd01
Wishazm <117849362+Wishazm@users.noreply.github.com>
2025-01-12 17:44:15 +0100

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,13 +5,13 @@ $('body').append('<p>ALX Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - ALX</p>');
+$('body').append('<p>Copyright - ALX</p>'); // Ensure this matches exactly
 
 let count = 0;
 
 function updateCounter() {
   count++;
-  $("#count").html(`${count} clicks on the button`);
+  $('#count').html(`${count} clicks on the button`);
 }
 
 $('button').on('click', _.debounce(updateCounter, 500));
