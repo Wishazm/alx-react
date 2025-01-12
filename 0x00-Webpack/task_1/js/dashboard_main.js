d255f18b711787f40d8f7f90773bf6da6f2d2d52
Wishazm <117849362+Wishazm@users.noreply.github.com>
2025-01-12 17:45:37 +0100

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require('jquery');
+const _ = require('lodash');
 
-$('body').append('<p>ALX Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - ALX</p>'); // Ensure this matches exactly
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+const updateCounter = () => {
+	let times = $('#count').html() || 0;
+	$('button').on('click', () => {
+		times++;
+		$('#count').html(`${times} clicks on the button`);
+	});
+};
 
-function updateCounter() {
-  count++;
-  $('#count').html(`${count} clicks on the button`);
-}
-
-$('button').on('click', _.debounce(updateCounter, 500));
+_.debounce(updateCounter, 500);
+updateCounter();
